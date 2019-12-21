2b2ea380bfe11e906771aa593b6c920b9fcf1184
TeddyJubu <syeedmdjobayer@gmail.com>
2019-12-21 21:56:46 +0600

    fizzbuzz  added

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzBuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "fizzbuzz"
+    elif number % 3 == 0:
+        return "fizz"
+    elif number % 5 == 0:
+        return "buzz"
+    
+    else:
+        return number
+
+
+
+print(fizzBuzz(15))
